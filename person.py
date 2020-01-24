8cbccf8d10330195fa4a4fb12ccce2f5c6396537
wis6d <wisteria6d@gmail.com>
2020-01-24 20:09:37 +0900

    Create person.py

---
+++ person.py
@@ -0,0 +1,18 @@
+class Person:
+  name = 'no name'
+  mail = 'no address'
+  age = 0
+
+  def say(self):
+    print('name:' + self.name + ' mail:' \
+      + self.mail + ' age:' + str(self.age))
+
+me = Person()
+print(me.name, me.mail, me.age)
+me.say()
+
+me.name = 'taro'
+me.mail = 'taro@yamada'
+me.age = 39
+
+me.say()
