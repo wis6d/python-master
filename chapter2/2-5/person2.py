45451442001d9868b40d1b64f59af834d87b8e7c
wis6d <wisteria6d@gmail.com>
2020-01-27 21:05:30 +0900

    Update person2.py

--- chapter2/2-5/person2.py
+++ chapter2/2-5/person2.py
@@ -2,16 +2,18 @@ class Person:
   name = 'no name'
   mail = 'no address'
   age = 0
+  nationality = 'no nationality'
 
   def say(self):
-    print('name:' + self.name + ' mail:' + self.mail + ' age:' + str(self.age))
+    print('name:' + self.name + ' mail:' + self.mail + ' age:' + str(self.age) + ' nationality:' + self.nationality)
 
 me = Person()
-print(me.name, me.mail, me.age)
+print(me.name, me.mail, me.age, me.nationality)
 me.say()
 
 me.name = 'taro'
 me.mail = 'taro@yamada'
 me.age = 39
+me.nationality = 'Japan'
 
 me.say()
