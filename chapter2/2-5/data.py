5fa1d1b9985fe51d577688971a739ee6b15308b8
wis6d <wisteria6d@gmail.com>
2020-01-28 22:47:48 +0900

    Create data.py

---
+++ chapter2/2-5/data.py
@@ -0,0 +1,11 @@
+data = [
+  98, 23, 45, 56,
+  89, 87, 12, 34,
+]
+
+total = sum(data)
+ave = total / len(data)
+
+print('平均:' + str(ave))
+print('最小:' + str(min(data)))
+print('最大:' + str(max(data)))
