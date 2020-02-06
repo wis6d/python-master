bf1577173a4c1c21f725de8f491b54bd55170765
wis6d <wisteria6d@gmail.com>
2020-02-06 21:30:56 +0900

    Create uppercase.py

---
+++ chapter3/3-3/uppercase.py
@@ -0,0 +1,19 @@
+s = '''
+One Little, two little, three little Indians
+Four little, five LITTLE, six LiTTle Indians(f)
+Seven LittlE, eight little, nine LittLe Indians
+Ten Little Indians.
+'''
+
+f = "little"
+r = "BIG"
+s2 = s.lower()
+n = 0
+
+while (s2.find(f, n) != -1):
+  i = s2.find(f, n)
+  s = s[:i] + r + s[(i + len(f)):]
+  s2 = s2[:i] + r + s2[(i + len(f)):]
+  n = i + len(r)
+
+print(s)
