2cbc56c1fcbd3f903d0b83c116e822e665ea5183
wis6d <wisteria6d@gmail.com>
2020-02-09 17:41:43 +0900

    Create arr_list.py

---
+++ chapter3/3-4/arr_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 17:38:28 2020
+
+@author: hideyuki.suzuki
+"""
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50])
+arr2 = np.array([5, 10, 15, 20, 25])
+print(arr1)
+print(arr2)
+print(arr1 + arr2)
+print(arr1 * arr2)
